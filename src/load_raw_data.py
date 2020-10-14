e7066afd2dd6656caebbd1ec94867c67b1288706
Daniel Adornes <daniel.adornes@gmail.com>
2020-10-13 23:35:38 -0300

    Removing unused imports

--- src/load_raw_data.py
+++ src/load_raw_data.py
@@ -1,5 +1,4 @@
 import gdown
-import numpy as np
 import pandas as pd
 
 from config import Config
--- src/train_model.py
+++ src/train_model.py
@@ -4,7 +4,6 @@ import pickle
 import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_log_error
 
 from config import Config
 
